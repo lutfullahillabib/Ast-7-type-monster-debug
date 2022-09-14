54607a616aa594d9d37f6ac8ccee9a97db49102b
lutfullahillabib <lutfullahillabib@gmail.com>
2022-09-15 01:35:37 +0600

    All error handle Done

--- script.js
+++ script.js
@@ -72,7 +72,9 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt((finishTime - startTime) / 1000);
+  // Math.round()
+  // Math.floor()
 
   // show result modal
   resultModal.innerHTML = "";
@@ -85,7 +87,7 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${parseInt(timeTaken)}</span> seconds</p>
+    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
@@ -103,14 +105,9 @@ const closeModal = () => {
   modalBackground.classList.toggle("hidden");
   resultModal.classList.toggle("hidden");
 
-  window.location.href = "index.html";
+
   // window.location.reload();
-  // window.location.replace("index.html");
 
-  // future-try-check
-
-  // window.location.search
-  // window.location.origin
 };
 
 const start = () => {
@@ -146,8 +143,11 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpent = parseInt((currentTime - startTime) / 1000);
+  // Math.round() 
+  // Math.floor()
 
+  // console.log(timeSpent);
 
-  document.getElementById("show-time").innerHTML = `${parseInt(startTime) ? parseInt(timeSpent) : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
 }, 1000);
